cfe0760bc5816d4dd0c48e3a910eae1c109db3d3
Marcos Souza Brito <82009172+marcos2872@users.noreply.github.com>
2025-12-26 12:56:35 -0300

    update

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo dnf install -y -q flatpak 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 source ~/.bashrc
